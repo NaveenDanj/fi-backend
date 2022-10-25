5057afa291d84655f906a77b6dbe6c4450076b99
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-10-25 10:20:57 +0530

    added file upload trait

---
+++ app/Traits/Upload.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+trait Upload
+{
+    public function UploadFile(UploadedFile $file, $folder = null, $disk = 'public', $filename = null)
+    {
+        $FileName = !is_null($filename) ? $filename : Str::random(10);
+        return $file->storeAs(
+            $folder,
+            $FileName . "." . $file->getClientOriginalExtension(),
+            $disk
+        );
+    }
+
+    public function deleteFile($path, $disk = 'public')
+    {
+        Storage::disk($disk)->delete($path);
+    }
+}
