0a5489a9670fb9a079bb886bcb14485ac3ffe7a0
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2023-01-09 04:46:53 +0530

    removed middlware from route

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,7 @@ Route::prefix('submission')->group(function (){
 
 Route::prefix('introducer')->group(function (){
     Route::middleware(['auth:sanctum' , 'abilities:admin' , 'roleAdminRequired'])->post('/add-introducer' , [IntroducerController::class , 'addIntroducer'] );
-    Route::middleware(['auth:sanctum' , 'abilities:admin' , 'roleAdminRequired'])->delete('/delete-introducer' , [AdminAuthController::class , 'deleteIntroducer'] );
+    Route::middleware(['auth:sanctum' , 'abilities:admin'])->delete('/delete-introducer' , [AdminAuthController::class , 'deleteIntroducer'] );
 });
 
 Route::prefix('meta')->group(function (){
