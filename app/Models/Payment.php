fe8da1d5a50ca5cb08bbf09f0fbdd397960a3b37
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2023-01-11 12:52:27 +0530

    added Payment model and migration

---
+++ app/Models/Payment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'referee_id',
+        'type',
+        'amount',
+        'status',
+        'checked_by'
+    ];
+
+}
---
+++ database/migrations/2023_01_11_071811_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('referee_id');
+            $table->string('type' , 10);
+            $table->decimal('amount' , 19 , 2);
+            $table->string('status' , 20);
+            $table->bigInteger('checked_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
