0a3b2df446b45c07972499ec15fe150bb16c5510
muditha41 <muditha41@gmail.com>
2023-03-28 13:10:42 +0400

    sendPushMessageToWeb method update

--- app/Http/Controllers/CustomerSubmissionController.php
+++ app/Http/Controllers/CustomerSubmissionController.php
@@ -58,7 +58,7 @@ class CustomerSubmissionController extends Controller
         ]);
 
         if ($response->getStatusCode() === 200) {
-            $introducer -> getRefreeIntroducer(user()->id);
+            $introducer = $this->getRefreeIntroducer($request->user()->id);
           //  sendPushMessageToWeb('New Submission updated!','New Submission ','');
         }
 
