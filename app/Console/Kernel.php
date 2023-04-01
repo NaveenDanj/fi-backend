73303bdfee444a3eedc2e2a4eb837a5fc234b094
muditha41 <muditha41@gmail.com>
2023-04-01 11:37:40 +0400

    shedule checkSubmissionStatusIsSubmitted function

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call('App\Http\Controllers\CustomerSubmissionController@checkSubmissionStatusIsSubmitted')
-        ->everyMinute();
+        ->daily();
         // $schedule->command('inspire')->hourly();
     }
 
